AIProphet/elasticluster

commit ce0d91382e954cf6f8d73bfa53315113222df772

Reword (some) help text and docstrings.

elasticluster/main.py
@@ -1,6 +1,6 @@
 #! /usr/bin/env python
 #
-#   Copyright (C) 2013-2014 S3IT, University of Zurich
+#   Copyright (C) 2013-2015 S3IT, University of Zurich
 #
 #   This program is free software: you can redistribute it and/or modify
 #   it under the terms of the GNU General Public License as published by
@@ -89,9 +89,10 @@ class ElastiCluster(cli.app.CommandLineApp):
                             Configurator.default_storage_path,
                        default=Configurator.default_storage_path)
         self.add_param('-c', '--config', metavar='PATH',
-                       help="Path to the configuration file. Default: `%s`. "
-                       "If directory PATH.d, also all files like "
-                       "PATH.d/*.conf are parsed." % self.default_configuration_file,
+                       help=("Path to the configuration file; default: `%s`. "
+                            "If directory `PATH.d` exists, also all files matching"
+                            " pattern `PATH.d/*.conf` are parsed."
+                             % self.default_configuration_file),
                        default=self.default_configuration_file)
         self.add_param('--version', action='store_true',
                        help="Print version information and exit.")
@@ -163,14 +164,13 @@ class ElastiCluster(cli.app.CommandLineApp):
 
     def main(self):
         """
-        Elasticluster will start, stop, grow, shrink clusters on an EC2 cloud.
-        """
-        # This is the main entry point of the elasticluster.  First the
-        # central configuration is created, which can be altered through
-        # the command line interface. Then the given command from the
-        # command line interface is called.
+        This is the main entry point of the ElastiCluster CLI.
 
-        # call the subcommand function (ususally execute)
+        First the central configuration is created, which can be altered
+        through the command line interface. Then the given command from
+        the command line interface is called.
+        """
+        assert self.params.func, ("No subcommand defined in `ElastiCluster.main()")
         try:
             return self.params.func()
         except MultipleInvalid as ex:

commit 2798cc540c07dcd72e43958d708b87419d90903f

Restore the correct `--help` text.
This was inadvertently broken in commit ce0d913.

elasticluster/main.py
@@ -53,6 +53,7 @@ from elasticluster.migration_tools import MigrationCommand
 
 class ElastiCluster(cli.app.CommandLineApp):
     name = "elasticluster"
+    description = "Elasticluster will start, stop, grow, shrink clusters on an EC2 cloud."
 
     default_configuration_file = os.path.expanduser(
         "~/.elasticluster/config")